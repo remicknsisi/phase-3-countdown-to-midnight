ed1f0a46720bd2f3247139f5ca37fdf449cd7451
Sienna <remicknsisi@gmail.com>
2023-03-09 11:54:16 -0500

    commit tree a6d6af5eb1c2f368815b55ff4f33e531eb281fd7

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,18 @@
-#write your code here
+def countdown int
+    while int >= 1
+        puts "#{int} SECOND(S)!"
+    int -= 1
+    end
+    int = "HAPPY NEW YEAR!"
+    int 
+end
 
-def countdown
+def countdown_with_sleep int
+    while int >= 1
+        puts "#{int} SECOND(S)!"
+    int -= 1
+    sleep(1)
+    end
+    int = "HAPPY NEW YEAR!"
+    int 
 end
